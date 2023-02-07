02f029439b822fa8f76a2ae9a1049a8fa1c10040
Ken-18I <kbazant@uni.pe>
2023-02-07 12:33:21 -0500

    Carga de datos BBVA

---
+++ src/sistema/alumnosBL/alumnosBL.java
@@ -0,0 +1,31 @@
+package sistema.alumnosBL;
+
+public class alumnosBL {
+    int ID;
+    String Nombre, Correo;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+    
+}
